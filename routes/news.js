2a8fd2ba4d5e4990bdd92c0f131a7f3e4b2a173c
hebbar7 <hebbar7@gmail.com>
2023-05-05 16:09:39 -0700

    more changes

--- routes/news.js
+++ routes/news.js
@@ -8,7 +8,41 @@ import { getArticlesController } from "../news/controller/articlesController.js"
  *       get:
  *          tags:
  *             - NewsAPI
- *       summary: Get all articles
+ *          summary: Get all articles
+ *          responses:
+ *             '200':
+ *                description: Modified
+ *                content:
+ *                   application/json:
+ *                      schema: 
+ *                         type: object
+ *                         properties: 
+ *                            status:
+ *                               type: integer
+ *                               example: 200
+ *                            response:
+ *                               type: object
+ *                               properties:
+ *                                  totalArticles: 
+ *                                     type: integer
+ *                                     example: 200
+ *                                  articles: 
+ *                                     type: array
+ *                                     example: [
+ *                                        {
+ *                                           "title": "Google's Pixel 7 and 7 Pro’s design gets revealed even more with fresh crisp renders",
+ *                                           "description": "Now we have a complete image of what the next Google flagship phones will look like. All that's left now is to welcome them during their October announcement!",
+ *                                           "content": "Google’s highly anticipated upcoming Pixel 7 series is just around the corner, scheduled to be announced on October 6, 2022, at 10 am EDT during the Made by Google event. Well, not that there is any lack of images showing the two new Google phones",
+ *                                           "url": "https://www.phonearena.com/news/google-pixel-7-and-pro-design-revealed-even-more-fresh-renders_id142800",
+ *                                           "image": "https://m-cdn.phonearena.com/images/article/142800-wide-two_1200/Googles-Pixel-7-and-7-Pros-design-gets-revealed-even-more-with-fresh-crisp-renders.jpg",
+ *                                           "publishedAt": "2022-09-28T08:14:24Z",
+ *                                           "source": {
+ *                                              "name": "PhoneArena",
+ *                                              "url": "https://www.phonearena.com"
+ *                                           }
+ *                                        }
+ *                                     ]
+ * 
  *       parameters:
  *          - in: query
  *            name: q
@@ -36,19 +70,6 @@ import { getArticlesController } from "../news/controller/articlesController.js"
  *            description: maximum number of records to return
  *            schema:
  *             type: integer
- *       responses:
- *          200:
- *             description: Modified
- *          400:
- *             description: Bad Request
- *          403:
- *             description: Forbidden
- *          429:
- *             description: Too many requests
- *          500:
- *             description: Internal Server Error
- *          503:
- *             description: Service Unavailable
  */
     router.get('/articles', getArticlesController)
 
