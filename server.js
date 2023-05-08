dff23e8bc7036bdd8e533025d11fe161a18b2caf
hebbar7 <hebbar7@gmail.com>
2023-05-08 10:32:27 -0700

    added cache

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express'
+import apicache from 'apicache'
 const app = express();
 import swaggerDocs from './swagger.js'
 import router from './routes/news.js'
 import { port } from './config.js';
 
+let cache = apicache.middleware
+
 app.use(router)
+app.use(cache())
 app.listen(port, () => {
   console.log(`API server listening on port ${port}`);
   swaggerDocs(app, port)
